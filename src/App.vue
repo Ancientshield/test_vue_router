aaf3c67065e3864bf5d455dd0a5caf67dc46dd5b
Ancientshield <ancientsword520@gmail.com>
2023-01-19 10:54:12 +0800

    trandition router-link router-vue

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup></script>
+
+<template>
+	<nav>
+		<router-link to="/">Go to Home</router-link>
+		<router-link to="/about">Go to About</router-link>
+	</nav>
+	<router-view></router-view>
+</template>
+
+<style scoped></style>
